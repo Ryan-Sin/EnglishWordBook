b6322083a01ce3ac6e48c77c2dead433cda7f1f8
Ryan-Sin <ryanshin937@gmail.com>
2020-09-14 23:50:18 +0900

    first commit

---
+++ EnglishWordBook/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  EnglishWordBook
+//
+//  Created by 신한국 on 2020/09/13.
+//  Copyright © 2020 Ryan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewConroller : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Do any additional setup after loading the view.
+    }
+    
+    
+    //로그인 클릭
+    @IBAction func loginAction(_ sender: Any) {
+    
+        print("로그인 클릭!!")
+    }
+    
+    //회원가입 클릭
+    @IBAction func memberAction(_ sender: Any) {
+        print("회원가입 클릭")
+    }
+}
---
+++ EnglishWordBook/MainTabBarController.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewController.swift
+//  EnglishWordBook
+//
+//  Created by 신한국 on 2020/09/13.
+//  Copyright © 2020 Ryan. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController : UITabBarController {
+    
+}
